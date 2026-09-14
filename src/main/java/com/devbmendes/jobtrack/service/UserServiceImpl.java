efeaaf0d1feac4b91058e1e28d0eb081002d0a3f
Balduino Mendes <baldhuino@gmail.com>
2026-09-14 13:42:03 +0100

    userRepository like a final variable

--- src/main/java/com/devbmendes/jobtrack/service/UserServiceImpl.java
+++ src/main/java/com/devbmendes/jobtrack/service/UserServiceImpl.java
@@ -2,20 +2,16 @@ package com.devbmendes.jobtrack.service;
 
 import com.devbmendes.jobtrack.entity.User;
 import com.devbmendes.jobtrack.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService{
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public UserServiceImpl(UserRepository userRepository){
         this.userRepository = userRepository;
     }
-    public UserServiceImpl(){
-
-    }
-
-    public void setUserRepository(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    
     public UserRepository getUserRepository(){
         return userRepository;
     }
