8e6b801b16b9427f8a13edbee0c5f3a02cd19dff
Balduino Mendes <baldhuino@gmail.com>
2026-09-14 13:03:26 +0100

    feat: add user entity and role

---
+++ src/main/java/com/devbmendes/jobtrack/entity/User.java
@@ -0,0 +1,73 @@
+package com.devbmendes.jobtrack.entity;
+
+import com.devbmendes.jobtrack.enums.Role;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    private LocalDateTime createdAt;
+
+
+    User(){
+
+    }
+    User(String name,String email,String password,Role role){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.createdAt = LocalDateTime.now();
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
---
+++ src/main/java/com/devbmendes/jobtrack/enums/Role.java
@@ -0,0 +1,5 @@
+package com.devbmendes.jobtrack.enums;
+
+public enum Role {
+    USER,ADMIN
+}
