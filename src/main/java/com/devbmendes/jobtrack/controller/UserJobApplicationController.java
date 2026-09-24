611c2dfeb8c6ae3fc2fab81c788f49fea5d6d8c8
Balduino Mendes <baldhuino@gmail.com>
2026-09-24 01:06:28 +0100

    delete some issues from entyties

---
+++ src/main/java/com/devbmendes/jobtrack/controller/UserJobApplicationController.java
@@ -0,0 +1,39 @@
+package com.devbmendes.jobtrack.controller;
+
+import com.devbmendes.jobtrack.dto.ApiResponse;
+import com.devbmendes.jobtrack.dto.UserJobAppResponse;
+import com.devbmendes.jobtrack.repository.UserJobApplicationRepository;
+import com.devbmendes.jobtrack.service.UserJobApplicationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/job-applications")
+public class UserJobApllicationController {
+    private final UserJobApplicationService userJobApplicationService;
+
+    public UserJobApllicationController(UserJobApplicationService userJobApplicationService) {
+        this.userJobApplicationService = userJobApplicationService;
+    }
+
+    @PostMapping("/{jobApplicationId}/apply/{userId}")
+    public ResponseEntity<ApiResponse<UserJobAppResponse>> apply(
+            @PathVariable Long jobApplicationId,
+            @PathVariable Long userId) {
+
+        UserJobAppResponse response =
+                userJobApplicationService
+                        .saveUserJobApplication(userId, jobApplicationId);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ApiResponse<>(
+                        "Application submitted successfully",
+                        response
+                ));
+    }
+}
